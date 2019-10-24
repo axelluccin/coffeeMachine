0b7994128997398deccd4feeb4e1f0dcc31d79af
Axel LUCCIN <axel.luccin@gmail.com>
2019-10-24 16:44:19 +0200

    test user order with one sugar

--- src/main/java/coffeeMachine/UserCommand.java
+++ src/main/java/coffeeMachine/UserCommand.java
@@ -3,11 +3,15 @@ package coffeeMachine;
 public class UserCommand {
     private final DrinkType drink;
 
-    public UserCommand(DrinkType drink) {
+    public UserCommand(DrinkType drink, int sugar) {
         this.drink = drink;
     }
 
     public String getDrink() {
         return "T";
+    }
+
+    public String getSugar() {
+        return "0";
     }
 }
--- src/test/java/acceptanceTest/CoffeeMachineAcceptanceTest.java
+++ src/test/java/acceptanceTest/CoffeeMachineAcceptanceTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CoffeeMachineAcceptanceTest {
     @Test
     public void when_customer_order_a_tea_with_sugar_then_coffee_machine_return_the_good_protocol() {
-        UserCommand userCommand = new UserCommand(DrinkType.TEA);
+        UserCommand userCommand = new UserCommand(DrinkType.TEA, 0);
         CoffeeMachine coffeeMachine = new CoffeeMachine();
 
         String resultCommand = coffeeMachine.command(userCommand);

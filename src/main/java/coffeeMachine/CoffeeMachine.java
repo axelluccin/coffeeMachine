3882d94bf90234a617a0a80754e3ae51b9bcd328
Axel LUCCIN <axel.luccin@gmail.com>
2019-10-25 15:17:06 +0200

    Refactor of some constant

--- src/main/java/coffeeMachine/CoffeeMachine.java
+++ src/main/java/coffeeMachine/CoffeeMachine.java
@@ -2,12 +2,12 @@ package coffeeMachine;
 
 public class CoffeeMachine {
 
-    private static final String MESSAGE_CONTENT = "M:message-content";
     private static final String SEPARATION = ":";
+    private static final String MESSAGE = "M:";
 
     public String order(UserOrder userOrder) {
         if (!userOrder.getMessage().isEmpty()) {
-            return "M:" + userOrder.getMessage();
+            return MESSAGE + userOrder.getMessage();
         }
         return userOrder.getDrink() + SEPARATION + userOrder.getSugar() + SEPARATION + userOrder.getStick();
     }

deaa3c4e6336b173a764767d4d8eb62170026384
Axel LUCCIN <axel.luccin@gmail.com>
2019-10-28 12:02:19 +0100

    Refactor format message to drink maker

--- src/main/java/coffeeMachine/ProtocolDrinkMaker.java
+++ src/main/java/coffeeMachine/ProtocolDrinkMaker.java
@@ -6,6 +6,8 @@ public class ProtocolDrinkMaker {
 
     private static final String SEPARATION = ":";
     private static final String MESSAGE = "M:";
+    private static final String IT_IS_MISSING = "M:It is missing ";
+    private static final String DOLLARS_TO_BUY_A = " dollars to buy a ";
 
     public String format(UserOrder userOrder) {
         return userOrder.getDrink() + SEPARATION + userOrder.getSugar() + SEPARATION + userOrder.getStick();
@@ -16,6 +18,6 @@ public class ProtocolDrinkMaker {
     }
 
     public String format(BigDecimal missingMoney, DrinkType drinkType) {
-        return MESSAGE + "It is missing " + missingMoney + " dollars to buy a " + drinkType.getDrink();
+        return IT_IS_MISSING + missingMoney + DOLLARS_TO_BUY_A + drinkType.getDrink();
     }
 }

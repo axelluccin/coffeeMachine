1756ef8200b1df2770fea5f75f77e6c31cf1309b
Axel LUCCIN <axel.luccin@gmail.com>
2019-10-28 16:51:26 +0100

    test if we had 2 coffee in list of 3 drink

--- src/main/java/coffeeMachine/ReportDrinkMachine.java
+++ src/main/java/coffeeMachine/ReportDrinkMachine.java
@@ -4,13 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReportDrinkMachine {
-    private List<Order> orders = new ArrayList<Order>();
+    private List<UserOrder> orders = new ArrayList<UserOrder>();
 
-    public List<Order> Orders() {
+    public List<UserOrder> Orders() {
         return orders;
     }
 
-    public void add(Order order) {
+    public void add(UserOrder order) {
         orders.add(order);
+    }
+
+    public int OrdersOfType(DrinkType drinkType) {
+        int count = 0;
+        for (UserOrder userOrder : orders) {
+            if (userOrder.getDrink().equals(drinkType)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
--- src/test/java/unitTest/ReportDrinkMachineTest.java
+++ src/test/java/unitTest/ReportDrinkMachineTest.java
@@ -1,10 +1,12 @@
 package unitTest;
 
-import coffeeMachine.Order;
+import coffeeMachine.DrinkType;
 import coffeeMachine.ReportDrinkMachine;
+import coffeeMachine.UserOrder;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,18 +21,31 @@ public class ReportDrinkMachineTest {
 
     @Test
     public void when_no_order_in_coffee_machine() {
-        List<Order> resultOrders = reportDrinkMachine.Orders();
+        List<UserOrder> resultOrders = reportDrinkMachine.Orders();
 
         assertThat(resultOrders.size()).isEqualTo(0);
     }
 
     @Test
-    public void when_one_orders_in_coffee_machine() {
-        Order order = new Order();
+    public void when_one_orders_in_coffee_machine_then_report_return_1_order() {
+        UserOrder order = new UserOrder(DrinkType.COFFEE, 0, BigDecimal.ZERO, false);
         reportDrinkMachine.add(order);
 
-        List<Order> resultOrders = reportDrinkMachine.Orders();
+        List<UserOrder> resultOrders = reportDrinkMachine.Orders();
 
         assertThat(resultOrders.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void when_2_orders_of_coffee_and_1_order_of_chocolate_then_report_return_2_orders() {
+        UserOrder orderCoffee = new UserOrder(DrinkType.COFFEE, 0, BigDecimal.ZERO, false);
+        UserOrder orderChocolate = new UserOrder(DrinkType.CHOCOLATE, 0, BigDecimal.ZERO, false);
+
+        reportDrinkMachine.add(orderChocolate);
+        reportDrinkMachine.add(orderCoffee);
+        reportDrinkMachine.add(orderCoffee);
+
+        int resultOrdersCoffee = reportDrinkMachine.OrdersOfType(DrinkType.COFFEE);
+        assertThat(resultOrdersCoffee).isEqualTo(2);
     }
 }

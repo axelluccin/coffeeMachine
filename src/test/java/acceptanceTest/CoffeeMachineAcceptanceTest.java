f8cd7f15916cb891042b0a3d8b562aa1c71e3448
Axel LUCCIN <axel.luccin@gmail.com>
2019-10-28 10:37:26 +0100

    Rename test method

--- src/test/java/acceptanceTest/CoffeeMachineAcceptanceTest.java
+++ src/test/java/acceptanceTest/CoffeeMachineAcceptanceTest.java
@@ -57,11 +57,13 @@ public class CoffeeMachineAcceptanceTest {
     }
 
     @Test
-    public void when_user_oder_a_tea_and_give_40_cents_then_the_drink_maker_should_return_the_string_protocol() {
+    public void when_user_oder_a_tea_and_give_30_cents_then_the_drink_maker_should_return_the_string_protocol() {
         UserOrder userOrder = new UserOrder(DrinkType.TEA, 0, BigDecimal.valueOf(0.3));
 
         coffeeMachine.order(userOrder);
 
         verify(drinkMaker).send("M:It missing 10 cents to buy a tea");
     }
+
+
 }
